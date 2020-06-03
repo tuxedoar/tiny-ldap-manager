d79dff2df38ea8a20fd4ad41a08b80227b01adbb
tuxedoar <tuxedoar@gmail.com>
2020-06-03 17:25:13 -0300

    minor fix on setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/tuxedoar/tiny-ldap-manager',
     author='tuxedoar',
     author_email='tuxedoar@gmail.com',
-    license='GPLv3'
+    license='GPLv3',
     packages=['tiny_ldap_manager'],
     python_requires='>=3.6',
     scripts=["tiny_ldap_manager/_version.py"],
